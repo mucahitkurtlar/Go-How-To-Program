b65f1640f0da3d3e5bddd6e02c9d2c5d010b00fb
mucahitkurtlar <mucahit@tutanota.com>
2020-03-31 22:48:36 +0300

    Add array

---
+++ array/array.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+  var arr0 [3]int
+  for i := 0; i < 3; i++{
+    arr0[i] = i
+  }
+  fmt.Println(arr0)
+
+  arr1 := []string{"a", "b", "c", "d", "e"}
+  fmt.Println(arr1)
+  var arr2 []string = arr1[1:3] //slice işlemi matematikteki [x, y) gibi gerçekleşiyor
+  fmt.Println(arr2)
+  fmt.Println(cap(arr2))
+  var arr3 []int
+  if arr3 == nil{
+    fmt.Println("Nil") //Array'lerin boş değeri nil'dir
+  }
+}
