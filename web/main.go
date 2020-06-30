9f5504086047d650c25b10a80ac8d956d237ee3d
Mucahit Kurtlar <mucahit@tutanota.com>
2020-06-30 10:15:20 +0300

    add http.FileServer()

--- web/main.go
+++ web/main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "index.html", nil)
-}
 
 var templates *template.Template
 
@@ -17,6 +14,19 @@ func main() {
 	templates = template.Must(template.ParseGlob("templates/*.html"))
 	r := mux.NewRouter()
 	r.HandleFunc("/", indexHandler).Methods("GET")
+	fs := http.FileServer(http.Dir("./static/"))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
 	http.Handle("/", r)
-	http.ListenAndServe(":8000", nil)
+	fmt.Println("Listening on port 8080")
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	var arr [2]string
+	arr[0] = r.URL.Path
+	arr[1] = "Yoğsa sen beni beğenmiyorsun?"
+	templates.ExecuteTemplate(w, "index.html", arr)
 }
